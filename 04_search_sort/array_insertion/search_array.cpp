674b1a4b73aa708bdd899bf2efeb8c3312e265c5
ADPotapov2004 <11h2w11@gmail.com>
2022-10-14 14:49:44 +0300

    feat: add func array search_positon

--- 04_search_sort/array_insertion/search_array.cpp
+++ 04_search_sort/array_insertion/search_array.cpp
@@ -1,15 +1,9 @@
 #include <iostream>
 using namespace std;
 const int size = 3;
-int *insert(int x,int p,int arr[]) {
-	int* arrTmp = new int[size + 1];
-	for (int i = 0; i < size; i++)
-		arrTmp[i] = arr[i];
-	for (int i = size; i > p; i--)
-		arrTmp[i] = arrTmp[i-1];
-	arrTmp[p] = x;
-	return arrTmp;
-}
+int* insert(int x, int p, int arr[]);
+
+int search_position(int x, int arr[]);
 int main()
 {
 	int A [size] = {1, 5, 8};
@@ -18,6 +12,24 @@ int main()
 	{
 		cout << res[i]<<" ";
 	}
+	cout << endl;
+	cout << search_position(7, A);
 
 	
+}
+int* insert(int x, int p, int arr[]) {
+	int* arrTmp = new int[size + 1];
+	for (int i = 0; i < size; i++)
+		arrTmp[i] = arr[i];
+	for (int i = size; i > p; i--)
+		arrTmp[i] = arrTmp[i - 1];
+	arrTmp[p] = x;
+	return arrTmp;
+}
+int search_position(int x, int arr[]) {
+	int i = 0;
+	while (arr[i] < x && i < size) {
+		i++;
+	}
+	return i;
 }
